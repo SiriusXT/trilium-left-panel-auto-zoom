d1bd7c5ceb4dc612aa282039595bcec2d278e29f
SiriusXT <1160925501@qq.com>
2023-03-02 17:02:19 +0800

    0.2

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@ Automatically widen note tree by moving your mouse. A convenient widget to navig
 
 When the tree on the left panel has a lot of long titles, it is not convenient to view. 
 This script can automatically increase the width of the left panel when the mouse moves to the left.
-## Version: 0.1
+## Version: 0.2
 ## Installation
 Create a code note of type JS Frontend with the contents of trilium-left-panel-auto-zoom.js and the label #widget
 ## Features
@@ -10,5 +10,6 @@ Create a code note of type JS Frontend with the contents of trilium-left-panel-a
  1. Automatically zoom in on the left panel when the mouse moves to the navigation bar. This is the default mode.
  2. Automatically zoom in on the left panel when the mouse moves to the left panel.
  3. Automatically zoom in on the left panel when the shortcut key `alt+s` is pressed. 
+When the left panel is hidden, the zoom operation automatically changes to show or hide the left panel. 
 ## Preview
 ![](./preview.gif)
--- trilium-left-panel-auto-zoom.js
+++ trilium-left-panel-auto-zoom.js
@@ -1,5 +1,5 @@
-// Version: 0.1
 $(document).ready(function() {
+    // version: 0.2
 	function showR() {
 		window.sf = new Array();
 		window.sf['model'] = 1 // Select the activation mode 
@@ -7,25 +7,52 @@ $(document).ready(function() {
  1. Automatically zoom in on the left panel when the mouse moves to the navigation bar. This is the default mode.
  2. Automatically zoom in on the left panel when the mouse moves to the left panel.
  3. Automatically zoom in on the left panel when the shortcut key `alt+s` is pressed. */
-		window.sf['launcher'] = document.querySelectorAll("div#launcher-pane.component")[0];
+		window.sf['scale']=20;  //Default zoom 20% 
+        window.sf['key']="alt+s"; //Only run when the mode = 3
+        
+        window.sf['launcher'] = document.querySelectorAll("div#launcher-container.component")[0];
 		window.sf['left'] = document.querySelectorAll("div#left-pane.component")[0];
 		window.sf['rest'] = document.querySelectorAll("div#rest-pane.component")[0];
+        window.sf['bx-chevrons'] = document.querySelectorAll("div#launcher-pane.component > button.button-widget.bx.component.launcher-button")[0];
+        window.sf['left_display'] = 1;//1: flex   0: none
+        if (window.sf['left'].style.display=="none") {
+            window.sf['left_display']=0;
+        }        
+        function change_left_display(){
+            window.sf['left_display']=(window.sf['left_display']+1)%2;
+        }
+        window.sf['bx-chevrons'].addEventListener("mouseover", function(){
+             window.sf['bx-chevrons'].addEventListener("click",change_left_display);
+            }
+        );
+        window.sf['bx-chevrons'].addEventListener("mouseout", function(){
+             window.sf['bx-chevrons'].removeEventListener("click",change_left_display);
+            }
+        );
+        
 		window.sf['inLeft'] = 199 // In Step 
-		window.sf['inRest'] = 0 // Out To what step 
+		window.sf['inRest'] = 0 // Out step 
 		window.sf['now'] = 0 // Odd number means it can be enlarged, and even number means it can be reduced 
-		window.sf['speed'] = [0.4, 0.8, 1.2, 1.5, 1.9, 2.2, 2.5, 2.8, 3.1, 3.4, 3.7, 3.9, 4.2, 4.4, 4.7, 4.9, 5.1, 5.4, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 6.9, 7.1, 7.3, 7.5, 7.6, 7.8, 8.0, 8.1, 8.3, 8.4, 8.6, 8.7, 8.9, 9.0, 9.2, 9.3, 9.4, 9.6, 9.7, 9.8, 10.0, 10.1, 10.2, 10.3, 10.4, 10.6, 10.7, 10.8, 10.9, 11.0, 11.1, 11.2, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12.0, 12.1, 12.2, 12.3, 12.4, 12.5, 12.5, 12.6, 12.7, 12.8, 12.9, 13.0, 13.1, 13.2, 13.3, 13.3, 13.4, 13.5, 13.6, 13.7, 13.8, 13.8, 13.9, 14.0, 14.1, 14.1, 14.2, 14.3, 14.4, 14.4, 14.5, 14.6, 14.7, 14.7, 14.8, 14.9, 14.9, 15.0, 15.1, 15.2, 15.2, 15.3, 15.4, 15.4, 15.5, 15.5, 15.6, 15.7, 15.7, 15.8, 15.9, 15.9, 16.0, 16.0, 16.1, 16.2, 16.2, 16.3, 16.3, 16.4, 16.5, 16.5, 16.6, 16.6, 16.7, 16.7, 16.8, 16.9, 16.9, 17.0, 17.0, 17.1, 17.1, 17.2, 17.2, 17.3, 17.3, 17.4, 17.4, 17.5, 17.5, 17.6, 17.7, 17.7, 17.8, 17.8, 17.8, 17.9, 17.9, 18.0, 18.0, 18.1, 18.1, 18.2, 18.2, 18.3, 18.3, 18.4, 18.4, 18.5, 18.5, 18.6, 18.6, 18.6, 18.7, 18.7, 18.8, 18.8, 18.9, 18.9, 19.0, 19.0, 19.0, 19.1, 19.1, 19.2, 19.2, 19.2, 19.3, 19.3, 19.4, 19.4, 19.5, 19.5, 19.5, 19.6, 19.6, 19.7, 19.7, 19.7, 19.8, 19.8, 19.8, 19.9, 19.9, 20.0, 20.0];
+        window.sf['speed']=[]
+        for(var i=0;i<200;i++){
+		window.sf['speed'].push(parseFloat((-window.sf['scale']/40000*(i-200)*(i-200)+window.sf['scale']).toFixed(1)));            
+        }
+        
 		// Listen to mouse events. Only one mouse event and keyboard event can be enabled 
 		switch (window.sf['model']) {
 			case 1:
 				{
 					window.sf['launcher'].addEventListener("mouseover", inLeft);
 					window.sf['rest'].addEventListener("mouseover", inRest);
+                    window.sf['bx-chevrons'].addEventListener("mouseover", inRest);
 					break;
 				}
 			case 2:
 				{
 					window.sf['left'].addEventListener("mouseover", inLeft);
+                    window.sf['launcher'].addEventListener("mouseover", inLeft);
 					window.sf['rest'].addEventListener("mouseover", inRest);
+                    window.sf['bx-chevrons'].addEventListener("mouseover", inRest);
 					break;
 				}
 			case 3:
@@ -37,10 +64,15 @@ $(document).ready(function() {
 							inRest();
 						}
 					}
-					api.bindGlobalShortcut("alt+s", suofang);
+					api.bindGlobalShortcut(window.sf['key'], suofang);
 					break;
 				}
 			default:
+                {
+                    window.sf['launcher'].addEventListener("mouseover", inLeft);
+					window.sf['rest'].addEventListener("mouseover", inRest);
+                    window.sf['bx-chevrons'].addEventListener("mouseover", inRest);
+                }
 		}
 	}
 	setTimeout(showR, 500);
@@ -57,6 +89,12 @@ function inLeft() {
 	} else {
 		window.sf['now'] += 1;
 	}
+    
+    if (window.sf['left_display']==0){
+        window.sf['bx-chevrons'].click();
+        return;
+    }
+    
 	// Hide the paragraph edit button 
 	var pb = document.getElementsByClassName("ck-button");
 	if (typeof(pb) != "undefined") {
@@ -84,14 +122,13 @@ function inLeft() {
 		window.sf['rest'].style.setProperty('width', "calc(" + window.sf['old_rest'] + "% - 2.5px)");
 		window.sf['leftw'] = window.sf['left'].style.width;
 		window.sf['restw'] = window.sf['rest'].style.width;
-		//console.log(window.sf['old_left']);
 	}
 	var benci = window.sf['now'];
-	for (var i = window.sf['inRest']; i < 200; i += 2) {
+	for (var i = window.sf['inRest']; i < 200; i += 1) {
 		(function(i) {
 			setTimeout(function() {
 				if (window.sf['now'] == benci) {
-					// 1 or 2 indicates whether the method or reduction is in progress. Only one thing can be done 
+                    // 1 or 2 indicates whether the method or reduction is in progress. Only one thing can be done 
 					window.sf['left'].style.setProperty('width', window.sf['leftw'].replace(window.sf['old_left'],
 						window.sf['old_left'] + window.sf['speed'][i]));
 					window.sf['rest'].style.setProperty('width', window.sf['restw'].replace(window.sf['old_rest'],
@@ -110,12 +147,17 @@ function inRest() {
 	} else {
 		window.sf['now'] += 1;
 	}
+    if (window.sf['left_display']==0){
+        window.sf['bx-chevrons'].click();
+        window.sf['left_display']=0;
+        return
+    }
 	var benci = window.sf['now'];
-	for (var i = 0; i < window.sf['inLeft'] + 1; i += 2) {
+	for (var i = 0; i < window.sf['inLeft'] + 1; i += 1) {
 		(function(i) {
 			setTimeout(function() {
-				if (window.sf['now'] == benci) {
-					window.sf['left'].style.setProperty('width', window.sf['leftw'].replace(window.sf['old_left'],
+				if (window.sf['now'] == benci) {  
+                    window.sf['left'].style.setProperty('width', window.sf['leftw'].replace(window.sf['old_left'],
 						window.sf['old_left'] + window.sf['speed'][window.sf['inLeft']] - window.sf['speed'][i]));
 					window.sf['rest'].style.setProperty('width', window.sf['restw'].replace(window.sf['old_rest'],
 						100 - (window.sf['old_left'] + window.sf['speed'][window.sf['inLeft']] - window.sf['speed'][i])));
