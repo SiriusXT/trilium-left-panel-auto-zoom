89b9ad1e197b306bbb9f570434053b0175daf8ef
SiriusXT <1160925501@qq.com>
2025-05-10 10:12:42 +0800

    0.3

--- readme.md
+++ readme.md
@@ -1,17 +1,8 @@
-Automatically widen note tree by moving your mouse. A convenient widget to navigate through notes with long titles and deep level notes.
-
-When the tree on the left panel has a lot of long titles, it is not convenient to view. 
-This script can automatically increase the width of the left panel when the mouse moves to the left.
-## Update
-Increase the setting: when moving to the left, the mouse needs to hover for a while, which can prevent accidental mouseEnter.
-## Version: 0.2.2
+## Version: 0.3
 ## Installation
 Create a code note of type JS Frontend with the contents of trilium-left-panel-auto-zoom.js and the label #widget
 ## Features
  Three modes are supported :
- 1. Automatically zoom in on the left panel when the mouse moves to the navigation bar. This is the default mode.
- 2. Automatically zoom in on the left panel when the mouse moves to the left panel.
- 3. Automatically zoom in on the left panel when the shortcut key `alt+s` is pressed. 
-When the left panel is hidden, the zoom operation automatically changes to show or hide the left panel. 
+ 1. When the mouse clicks on the launcher bar, the left pane will automatically expand to the maximum.
 ## Preview
 ![](./preview.gif)
--- trilium-left-panel-auto-zoom.js
+++ trilium-left-panel-auto-zoom.js
@@ -1,207 +1,133 @@
-// version: 0.2.2
-var sf = new Array();
-$(document).ready(function () {
-	function showR() {
-		sf['model'] = 1; // Select the activation mode 
-		/*  Three modes are supported :
-		1. Automatically zoom in on the left panel when the mouse moves to the navigation bar. This is the default mode.
-		2. Automatically zoom in on the left panel when the mouse moves to the left panel.
-		3. Automatically zoom in on the left panel when the shortcut key `alt+s` is pressed. */
-		sf['scale'] = 20; //Default zoom 20% 
-		sf['key'] = "alt+x"; //Only run when the mode = 3
-		sf['delay'] = 150 //The event that the mouse needs to stay when the mouse moves in, default:100ms
+// version: 0.3
+const styles = `
+/* Pop up display tree */
+.popup-style {
+    position: fixed !important;
+    z-index: 100 !important;
+    background: var(--left-pane-background-color, #f0f0f0) !important;
+  }
+  .dropdown-menu-left {
+  	z-index: 101 !important;
+  }
+  `;
 
-		sf['launcher'] = document.querySelectorAll("div#launcher-container.component")[0];
-		sf['left'] = document.querySelectorAll("div#left-pane.component")[0];
-		sf['rest'] = document.querySelectorAll("div#rest-pane.component")[0];
-		sf['bx-chevrons'] = document.querySelectorAll("div#launcher-pane.component > button.button-widget.bx.component.launcher-button")[0];
-		sf['left_display'] = 1; //1: flex   0: none
-		sf['mmx'] = 0;
-		if (sf['left'].style.display == "none") {
-			sf['left_display'] = 0;
-		}
+let style = document.createElement('style');
+style.innerHTML = styles;
+document.head.appendChild(style);
 
-		function change_left_display() {
-			sf['left_display'] = (sf['left_display'] + 1) % 2;
-		}
-		sf['bx-chevrons'].addEventListener("mouseover", function () {
-			sf['bx-chevrons'].addEventListener("click", change_left_display);
-		});
-		sf['bx-chevrons'].addEventListener("mouseout", function () {
-			sf['bx-chevrons'].removeEventListener("click", change_left_display);
-		});
-
-		sf['inLeft'] = 199 // In Step 
-		sf['inRest'] = 0 // Out step 
-		sf['now'] = 0 // Odd number means it can be enlarged, and even number means it can be reduced 
-		sf['speed'] = []
-		for (var i = 0; i < 200; i++) {
-			sf['speed'].push(parseFloat((-sf['scale'] / 40000 * (i - 200) * (i - 200) + sf['scale'])
-				.toFixed(1)));
-		}
-
-		// Listen to mouse events. Only one mouse event and keyboard event can be enabled 
-		switch (sf['model']) {
-			case 1:
-				{
-					var timeoutEnter;
-					$(sf['launcher']).mouseenter(function () {
-						clearTimeout(timeoutEnter);
-						timeoutEnter = setTimeout(function () {
-							inLeft();
-						}, sf['delay']);
-					}).mouseleave(function () {
-                        clearTimeout(timeoutEnter); 
-                    });
-
-					sf['rest'].addEventListener("mouseover", inRest);
-					sf['bx-chevrons'].addEventListener("mouseover", inRest);
-					break;
-				}
-			case 2:
-				{
-                    var timeoutEnter;
-					$(sf['left']).mouseenter(function () {
-						clearTimeout(timeoutEnter);
-						timeoutEnter = setTimeout(function () {
-							inLeft();
-						}, sf['delay']);
-					}).mouseleave(function () {
-                        clearTimeout(timeoutEnter); 
-                    });
-                    			/*		var timeoutEnter;
-					$(sf['launcher']).mouseenter(function () {
-						clearTimeout(timeoutEnter);
-						timeoutEnter = setTimeout(function () {
-							inLeft();
-						}, sf['delay']);
-					})
-					*/
-					sf['rest'].addEventListener("mouseover", inRest);
-					sf['bx-chevrons'].addEventListener("mouseover", inRest);
-					break;
-				}
-			case 3:
-				{
-					function suofang() {
-						if (sf['now'] % 2 == 0) {
-							inLeft();
-						} else {
-							inRest();
-						}
-					}
-					api.bindGlobalShortcut(sf['key'], suofang);
-					break;
-				}
-			default:
-				{
-					sf['launcher'].addEventListener("mouseover", inLeft);
-					sf['rest'].addEventListener("mouseover", inRest);
-					sf['bx-chevrons'].addEventListener("mouseover", inRest);
-				}
-		}
+let mouseInLeft = false;
+let mouseInlauncherPane = false;
+let leftPane, tree, menu, lb, launcherPane, restPane;
+let needHiddenleft = false;
+function disablePop() {
+	if (tree !== undefined) {
+		tree.classList.remove("popup-style");
 	}
-	setTimeout(showR, 500);
-});
-
-function inLeft() {
-	if (sf['now'] == 10001) {
-		sf['now'] = 1;
-	} else if (sf['now'] == 10000) {
-		sf['now'] = 0;
+	if (needHiddenleft) {
+		if (lb.classList.contains('bx-chevrons-left')) lb.click();
 	}
-	if (sf['now'] % 2 == 1) {
-		return;
-	} else {
-		sf['now'] += 1;
+	needHiddenleft = false;
+}
+function enablePop() {
+	if (tree !== undefined) {
+		tree.classList.add("popup-style");
 	}
-
-	if (sf['left_display'] == 0) {
-		sf['bx-chevrons'].click();
-		return;
+	if (lb.classList.contains('bx-chevrons-right')) {
+		lb.click();
+		needHiddenleft = true;
 	}
-
-	// Hide the paragraph edit button 
-	var pb = document.getElementsByClassName("ck-button");
-	if (typeof (pb) != "undefined") {
-		var i;
-		for (i = 0; i < pb.length; i++) {
-			pb[i].style.display = "none";
-		}
-	}
-
-	if (sf['inRest'] == 0 || sf['inRest'] == 1) //0
-	{
-		// The original width is obtained only when it is fully returned 
-		sf['leftw'] = sf['left'].style.width;
-		sf['restw'] = sf['rest'].style.width;
-		sf['old_left'] = parseInt(sf['leftw'].substring(5, 7));
-		sf['old_rest'] = parseInt(sf['restw'].substring(5, 7));
-		if (sf['leftw'].charAt(7) != "%") {
-			if (parseInt(sf['leftw'].charAt(7)) < 5) {
-				sf['old_rest'] += 1;
-			} else {
-				sf['old_left'] += 1;
-			}
-		}
-		sf['left'].style.setProperty('width', "calc(" + sf['old_left'] + "% - 2.5px)");
-		sf['rest'].style.setProperty('width', "calc(" + sf['old_rest'] + "% - 2.5px)");
-		sf['leftw'] = sf['left'].style.width;
-		sf['restw'] = sf['rest'].style.width;
-	}
-	var benci = sf['now'];
-	for (var i = sf['inRest']; i < 200; i += 1) {
-		(function (i) {
-			setTimeout(function () {
-				if (sf['now'] == benci) {
-					// 1 or 2 indicates whether the method or reduction is in progress. Only one thing can be done 
-					sf['left'].style.setProperty('width', sf['leftw'].replace(sf['old_left'],
-						sf['old_left'] + sf['speed'][i]));
-					sf['rest'].style.setProperty('width', sf['restw'].replace(sf['old_rest'],
-						100 - (sf['old_left'] + sf['speed'][i])));
-					sf['inLeft'] = i;
-					// What is the current step 
-				}
-			}, (i + 1));
-		})(i)
-	}
+	setTimeout(() => {
+		leftPane.querySelector(".scroll-to-active-note-button").click();
+	}, 200);
 }
 
-function inRest() {
-	if (sf['now'] % 2 == 0) {
-		return;
-	} else {
-		sf['now'] += 1;
+function getEle() {
+	leftPane = document.querySelector("#left-pane");
+	if (!leftPane) {
+		return false;
 	}
-	if (sf['left_display'] == 0) {
-		sf['bx-chevrons'].click();
-		sf['left_display'] = 0;
-		return
+	tree = leftPane.querySelector(".tree");
+	if (!tree) {
+		return false;
 	}
-	// show the paragraph edit button 
-	setTimeout(function () {
-		var pb = document.getElementsByClassName("ck-button");
-		if (typeof (pb) != "undefined") {
-			var i;
-			for (i = 0; i < pb.length; i++) {
-				pb[i].style.display = "inline-flex";
-			}
+	menu = leftPane.querySelector("#context-menu-container");
+	restPane = document.querySelector("#rest-pane");
+	launcherPane = document.querySelector("#launcher-pane");
+	lb = launcherPane.querySelector(".launcher-button.bx-chevrons-right, .launcher-button.bx-chevrons-left");
+	if (!leftPane || !tree || !launcherPane) {
+		return false;
+	}
+	// Monitor mouse
+	leftPane.addEventListener("mouseenter", () => {
+		mouseInLeft = true;
+	});
+	leftPane.addEventListener("mouseleave", () => {
+		mouseInLeft = false;
+	});
+	launcherPane.addEventListener("mouseenter", () => {
+		mouseInlauncherPane = true;
+	});
+	launcherPane.addEventListener("mouseleave", () => {
+		mouseInlauncherPane = false;
+	});
+	restPane.addEventListener("mouseenter", () => {
+		mouseInLeft = false;
+		disablePop();
+	});
 
+	// Monitor keyboard
+	let altKeyPressTime = 0; // Record Alt key press time
+	document.addEventListener('keydown', function (event) {
+		if (event.key === 'Alt' && altKeyPressTime === 0 && !event.ctrlKey && !event.shiftKey) {
+			altKeyPressTime = Date.now();
 		}
-	}, 100);
-
-	var benci = sf['now'];
-	for (var i = 0; i < sf['inLeft'] + 1; i += 1) {
-		(function (i) {
-			setTimeout(function () {
-				if (sf['now'] == benci) {
-					sf['left'].style.setProperty('width', sf['leftw'].replace(sf['old_left'],
-						sf['old_left'] + sf['speed'][sf['inLeft']] - sf['speed'][i]));
-					sf['rest'].style.setProperty('width', sf['restw'].replace(sf['old_rest'],
-						100 - (sf['old_left'] + sf['speed'][sf['inLeft']] - sf['speed'][i])));
-					sf['inRest'] = 199 - i;
+	});
+	document.addEventListener('keyup', function (event) {
+		if (event.key === 'Alt' && altKeyPressTime > 0) {
+			const pressDuration = Date.now() - altKeyPressTime; // Calculate press duration
+			altKeyPressTime = 0;
+			if (pressDuration < 200) {
+				if (mouseInLeft || mouseInlauncherPane) {
+					if (tree.classList.contains("popup-style")) {
+						disablePop();
+					} else {
+						enablePop();
+					}
 				}
-			}, (i + 1));
-		})(i)
+			}
+		}
+	});
+
+	setTimeout(() => {
+		$(launcherPane).find('.spacer.component').on('click', (event) => {
+			if (tree !== undefined) {
+				if (tree.classList.contains("popup-style")) {
+					disablePop();
+				} else {
+					enablePop();
+				}
+			}
+		});
+	}, 500)
+	return true;
+}
+
+setTimeout(() => {
+	if (!getEle()) {
+		setTimeout(() => {
+			if (!getEle()) {
+				setTimeout(() => {
+					getEle();
+				}, 3000)
+			}
+		}, 1000)
 	}
+}, 200)
+
+module.exports = class extends api.NoteContextAwareWidget {
+	static get parentWidget() { return 'note-detail-pane'; }
+	constructor() { super(); }
+	isEnabled() { return true; }
+	doRender() { this.$widget = $(''); return this.$widget; }
+	async refreshWithNote() { disablePop(); }
 }
